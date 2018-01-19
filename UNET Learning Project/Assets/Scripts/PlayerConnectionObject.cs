68d6af1b1f19c90ecea12867e20e2f70541531c4
phillserrazina <fserrazina99@gmail.com>
2018-01-19 00:55:47 +0000

    Abstract

---
+++ UNET Learning Project/Assets/Scripts/PlayerConnectionObject.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerConnectionObject : NetworkBehaviour {
+
+	// VARIABLES
+
+	// SyncVars are variables that transmits data changes from the server to the clients.
+	[SerializeField, SyncVar(hook="OnPlayerNameChange")]
+	private string PlayerName = "Default";
+
+	public GameObject PlayerUnitPrefab;
+
+	// FUNCTIONS
+
+	// Use this for initialization
+	void Start () 
+	{
+		// Check Owner
+		if(isLocalPlayer == false)
+		{
+			return;
+		}
+
+		// Command the server to spawn unit
+		CmdSpawnMyUnit();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(isLocalPlayer == false)
+		{
+			return;
+		}
+
+		if(Input.GetKeyDown(KeyCode.S))
+		{
+			CmdSpawnMyUnit ();
+		}
+
+		if(Input.GetKeyDown(KeyCode.N))
+		{
+			string s = "Player" + Random.Range (1, 100);
+
+			CmdChangePlayerName (s);
+		}
+	}
+
+	void OnPlayerNameChange(string newName)
+	{
+		Debug.Log ("OnPlayerNameChange: " + newName);
+
+		// If a hook is used on a SyncVar, the local value does NOT get
+		// automatically update.
+		PlayerName = newName;
+
+		gameObject.name = newName;
+	}
+
+	// COMANDS (Special functions that ONLY get executed on the server)
+
+	private GameObject myPlayerUnit;
+
+	[Command] // "Command" executes on the server, not the client
+	void CmdSpawnMyUnit()
+	{
+		GameObject gameObject = Instantiate (PlayerUnitPrefab);
+
+		myPlayerUnit = gameObject;
+
+//		gameObject.GetComponent<NetworkIdentity> ().AssignClientAuthority (connectionToClient);
+
+		NetworkServer.SpawnWithClientAuthority (gameObject, connectionToClient);
+	}
+
+	[Command]
+	void CmdMoveUnitUp()
+	{
+		if(myPlayerUnit == null)
+		{
+			return;
+		}
+
+		myPlayerUnit.transform.Translate (0, 1, 0);
+	}
+
+	[Command]
+	void CmdChangePlayerName(string s)
+	{
+		PlayerName = s;
+//		RpcChangePlayerName (PlayerName);
+	}
+
+	// RPCs (Special functions that ONLY get executed on the clients.
+
+//	[ClientRpc]
+//	void RpcChangePlayerName(string s)
+//	{
+//		PlayerName = s;
+//	}
+}
---
+++ UNET Learning Project/Assets/Scripts/PlayerUnit.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+ 
+// PlayerUnit is a unit controlled by the player
+
+public class PlayerUnit : NetworkBehaviour {
+
+	// VARIABLES
+
+	Vector3 velocity;
+
+	// Most correct position for this player
+	Vector3 bestGuessPosition;	
+
+	// This is a constantly updated value about our latency to the server.
+	// This should probably be something we get from the PlayerConnectionObject.
+	float ourLatency = 0f;	
+
+	float latencySmoothingFactor = 10;
+
+	// FUNCTIONS
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.Translate (velocity * Time.deltaTime);
+
+		if(hasAuthority == false)
+		{
+			bestGuessPosition = bestGuessPosition + (velocity * Time.deltaTime);
+
+			transform.position = Vector3.Lerp (transform.position, bestGuessPosition, Time.deltaTime * latencySmoothingFactor);
+
+			return;
+		}
+
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			this.transform.Translate (0, 1, 0);
+		}
+
+		if (true)
+		{
+			velocity = new Vector3 (1, 0, 0);
+
+			CmdUpdateVelocity (velocity, transform.position);
+		}
+
+	}
+
+	// COMMANDS
+
+	[Command]
+	void CmdUpdateVelocity(Vector3 v, Vector3 position)
+	{
+		// I am on a server
+		transform.position = position;
+		velocity = v;
+
+		// Knowing the latency:
+		// transform.position = position + (v * thisPlayersLatencyToServer);
+
+		RpcUpdateVelocity (velocity, transform.position);
+
+	}
+
+	[ClientRpc]
+	void RpcUpdateVelocity(Vector3 v, Vector3 position)
+	{
+		// I am on a client
+
+		if(hasAuthority == true)
+		{
+			return;
+		}
+
+		// transform.position = position;
+
+		velocity = v;
+		bestGuessPosition = position + (velocity * ourLatency);
+
+		// Knowing the latency:
+		// transform.position = position + (v * ourLatency);
+	}
+}
